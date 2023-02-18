2ab222880f13aebcb2c780ab9f224b2139e0b57d
Sanjeev Dasgupta <sanjeev_dasgupta@hotmail.com>
2023-02-18 20:54:05 +0530

    Add files via upload

---
+++ src/config/dummylist.js
@@ -0,0 +1,403 @@
+export const userlist = [
+	{
+		"name": "Neil Franco",
+		"email": "velit@hotmail.net"
+	},
+	{
+		"name": "Chester Cotton",
+		"email": "accumsan.laoreet@outlook.org"
+	},
+	{
+		"name": "Charles Blanchard",
+		"email": "vitae@hotmail.ca"
+	},
+	{
+		"name": "Axel Valencia",
+		"email": "sit@outlook.ca"
+	},
+	{
+		"name": "Isaiah Ray",
+		"email": "felis.adipiscing@google.com"
+	},
+	{
+		"name": "Lev Rocha",
+		"email": "erat.vitae.risus@google.net"
+	},
+	{
+		"name": "Ora Estrada",
+		"email": "duis.ac.arcu@protonmail.edu"
+	},
+	{
+		"name": "Travis Herring",
+		"email": "dictum.mi@protonmail.com"
+	},
+	{
+		"name": "Lilah Schmidt",
+		"email": "nullam.scelerisque@hotmail.couk"
+	},
+	{
+		"name": "Unity Guerrero",
+		"email": "adipiscing.fringilla@hotmail.com"
+	},
+	{
+		"name": "Dominique Frank",
+		"email": "proin@yahoo.org"
+	},
+	{
+		"name": "Kane Cruz",
+		"email": "ipsum.ac@outlook.com"
+	},
+	{
+		"name": "Ivory Medina",
+		"email": "quam.quis@google.com"
+	},
+	{
+		"name": "Kennedy Williamson",
+		"email": "sem.ut@aol.ca"
+	},
+	{
+		"name": "Hedda Leonard",
+		"email": "mauris.vestibulum@icloud.net"
+	},
+	{
+		"name": "Cameron Charles",
+		"email": "sed@protonmail.couk"
+	},
+	{
+		"name": "Glenna Conner",
+		"email": "nunc.in@outlook.com"
+	},
+	{
+		"name": "Ariel Atkins",
+		"email": "proin.velit@aol.net"
+	},
+	{
+		"name": "Chava Gibson",
+		"email": "integer.eu@aol.com"
+	},
+	{
+		"name": "Jin Roy",
+		"email": "mauris.molestie@protonmail.edu"
+	},
+	{
+		"name": "Mia Watson",
+		"email": "ipsum.primis@aol.edu"
+	},
+	{
+		"name": "Dana Weiss",
+		"email": "imperdiet.nec@icloud.com"
+	},
+	{
+		"name": "Rose Barr",
+		"email": "dis.parturient.montes@yahoo.org"
+	},
+	{
+		"name": "Basil Berger",
+		"email": "fringilla@google.couk"
+	},
+	{
+		"name": "Nash Forbes",
+		"email": "non@outlook.org"
+	},
+	{
+		"name": "Ciaran Delgado",
+		"email": "arcu@yahoo.edu"
+	},
+	{
+		"name": "Brenden Olsen",
+		"email": "magna.suspendisse.tristique@hotmail.couk"
+	},
+	{
+		"name": "Keefe Gould",
+		"email": "magna.lorem@hotmail.couk"
+	},
+	{
+		"name": "Ria Molina",
+		"email": "velit.in@hotmail.org"
+	},
+	{
+		"name": "Buffy Hays",
+		"email": "accumsan.neque@icloud.org"
+	},
+	{
+		"name": "Quail Larson",
+		"email": "interdum.nunc@google.org"
+	},
+	{
+		"name": "Clarke Camacho",
+		"email": "dui.cras@yahoo.com"
+	},
+	{
+		"name": "Whoopi Bonner",
+		"email": "sed.pharetra@aol.couk"
+	},
+	{
+		"name": "Bethany Vang",
+		"email": "cras.eu@google.couk"
+	},
+	{
+		"name": "Echo Flores",
+		"email": "etiam.vestibulum@hotmail.ca"
+	},
+	{
+		"name": "Rana Michael",
+		"email": "libero.donec@protonmail.ca"
+	},
+	{
+		"name": "Alana Mendez",
+		"email": "cras.eu@google.net"
+	},
+	{
+		"name": "Gray Blevins",
+		"email": "sed@aol.couk"
+	},
+	{
+		"name": "Brielle Carson",
+		"email": "risus.at.fringilla@yahoo.ca"
+	},
+	{
+		"name": "Neve Goff",
+		"email": "interdum.ligula@protonmail.couk"
+	},
+	{
+		"name": "Noel Joyner",
+		"email": "eros@google.edu"
+	},
+	{
+		"name": "Chadwick Shepherd",
+		"email": "magna.nam@icloud.edu"
+	},
+	{
+		"name": "Carly Holt",
+		"email": "metus.in.lorem@yahoo.ca"
+	},
+	{
+		"name": "Ainsley Dale",
+		"email": "ipsum.cursus@outlook.com"
+	},
+	{
+		"name": "Rahim Hensley",
+		"email": "ultricies.adipiscing.enim@hotmail.org"
+	},
+	{
+		"name": "Jaime Clark",
+		"email": "luctus.ipsum@aol.net"
+	},
+	{
+		"name": "Connor Macdonald",
+		"email": "lectus.nullam.suscipit@aol.couk"
+	},
+	{
+		"name": "Justina Sloan",
+		"email": "etiam.bibendum@protonmail.ca"
+	},
+	{
+		"name": "Frances Merrill",
+		"email": "libero@aol.org"
+	},
+	{
+		"name": "Fay Long",
+		"email": "montes.nascetur.ridiculus@icloud.edu"
+	},
+	{
+		"name": "Fitzgerald Carr",
+		"email": "nulla.magna@icloud.org"
+	},
+	{
+		"name": "Herman Myers",
+		"email": "nullam@hotmail.ca"
+	},
+	{
+		"name": "Nash Schultz",
+		"email": "tristique.senectus@aol.edu"
+	},
+	{
+		"name": "Hanna Clayton",
+		"email": "ullamcorper.duis@yahoo.ca"
+	},
+	{
+		"name": "Fitzgerald Carroll",
+		"email": "augue.ac@icloud.net"
+	},
+	{
+		"name": "Zeph Cotton",
+		"email": "magna.sed@outlook.net"
+	},
+	{
+		"name": "Hector Wall",
+		"email": "ipsum.leo@aol.edu"
+	},
+	{
+		"name": "Caesar Crawford",
+		"email": "tristique.senectus@outlook.net"
+	},
+	{
+		"name": "Benedict Curry",
+		"email": "porta.elit@hotmail.net"
+	},
+	{
+		"name": "Price Howe",
+		"email": "placerat.eget@outlook.couk"
+	},
+	{
+		"name": "Jena Rowe",
+		"email": "velit.pellentesque@icloud.org"
+	},
+	{
+		"name": "Alyssa Slater",
+		"email": "lorem@google.couk"
+	},
+	{
+		"name": "Forrest Hayden",
+		"email": "mauris.sagittis.placerat@google.edu"
+	},
+	{
+		"name": "Cheryl Mccullough",
+		"email": "aliquam.auctor@hotmail.org"
+	},
+	{
+		"name": "Rafael Kidd",
+		"email": "sagittis@aol.org"
+	},
+	{
+		"name": "Velma O'Neill",
+		"email": "id.magna.et@yahoo.com"
+	},
+	{
+		"name": "Minerva Dudley",
+		"email": "diam.sed.diam@yahoo.net"
+	},
+	{
+		"name": "Kirk James",
+		"email": "pellentesque.habitant@aol.org"
+	},
+	{
+		"name": "Athena Mcbride",
+		"email": "lorem.ipsum.dolor@outlook.couk"
+	},
+	{
+		"name": "Amity Keller",
+		"email": "sem.ut.dolor@protonmail.org"
+	},
+	{
+		"name": "Isabelle Fisher",
+		"email": "in.molestie@outlook.couk"
+	},
+	{
+		"name": "Ignatius Parsons",
+		"email": "nullam.enim.sed@outlook.ca"
+	},
+	{
+		"name": "Abra Walker",
+		"email": "vel.venenatis@icloud.edu"
+	},
+	{
+		"name": "Joan Figueroa",
+		"email": "orci@google.org"
+	},
+	{
+		"name": "Kadeem Bates",
+		"email": "consequat@icloud.com"
+	},
+	{
+		"name": "Yoshi Nelson",
+		"email": "cum.sociis@outlook.net"
+	},
+	{
+		"name": "Scarlet Ryan",
+		"email": "sed.nec.metus@icloud.ca"
+	},
+	{
+		"name": "Deacon Contreras",
+		"email": "pharetra.felis@yahoo.com"
+	},
+	{
+		"name": "Xaviera Wooten",
+		"email": "nibh.dolor.nonummy@hotmail.org"
+	},
+	{
+		"name": "Holmes Thornton",
+		"email": "morbi@outlook.couk"
+	},
+	{
+		"name": "Carissa Sanchez",
+		"email": "auctor.vitae@outlook.org"
+	},
+	{
+		"name": "Lillith Haley",
+		"email": "enim.non@yahoo.couk"
+	},
+	{
+		"name": "Christen Mcfadden",
+		"email": "ut.aliquam.iaculis@outlook.ca"
+	},
+	{
+		"name": "Abbot Alexander",
+		"email": "non@protonmail.couk"
+	},
+	{
+		"name": "Belle Ellis",
+		"email": "mauris.a@protonmail.net"
+	},
+	{
+		"name": "Lucy Dunn",
+		"email": "erat.sed.nunc@google.org"
+	},
+	{
+		"name": "Jessamine Mcgowan",
+		"email": "ante.blandit.viverra@icloud.org"
+	},
+	{
+		"name": "Jasper Booker",
+		"email": "vestibulum@yahoo.edu"
+	},
+	{
+		"name": "Zoe Goodman",
+		"email": "sagittis.nullam.vitae@hotmail.org"
+	},
+	{
+		"name": "Francesca Fulton",
+		"email": "tempus@aol.com"
+	},
+	{
+		"name": "Burke Schultz",
+		"email": "at.velit.pellentesque@outlook.org"
+	},
+	{
+		"name": "Chancellor Gibson",
+		"email": "quis.accumsan@yahoo.ca"
+	},
+	{
+		"name": "Adena Conner",
+		"email": "vestibulum@google.org"
+	},
+	{
+		"name": "Risa Sanford",
+		"email": "turpis.in.condimentum@google.edu"
+	},
+	{
+		"name": "Yuli Hutchinson",
+		"email": "a.enim.suspendisse@outlook.couk"
+	},
+	{
+		"name": "Judith Ingram",
+		"email": "sodales.at.velit@protonmail.com"
+	},
+	{
+		"name": "Channing Bray",
+		"email": "egestas.urna@outlook.net"
+	},
+	{
+		"name": "Daquan Nicholson",
+		"email": "diam.sed@protonmail.edu"
+	},
+	{
+		"name": "Ebony Price",
+		"email": "non.hendrerit.id@hotmail.edu"
+	},
+	{
+		"name": "Solomon Sampson",
+		"email": "donec.consectetuer.mauris@hotmail.ca"
+	}
+];
+
